cae71c276af2012d6894c775b0ee4343a751f1a7
kanwar Dhillon <kdhillon469@gmail.com>
2023-01-28 13:32:04 +1100

    controllers added

---
+++ RunGroupWebApp/Controllers/ClubController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RunGroupWebApp.Data;
+
+namespace RunGroupWebApp.Controllers
+{
+    public class ClubController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public ClubController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Index()
+        {
+            var clubs = _context.Clubs.ToList();
+            return View(clubs);
+        }
+    }
+}
--- RunGroupWebApp/Data/ApplicationDbContext.cs
+++ RunGroupWebApp/Data/ApplicationDbContext.cs
@@ -1,6 +1,17 @@
-﻿namespace RunGroupWebApp.Data
+﻿using Microsoft.EntityFrameworkCore;
+using RunGroupWebApp.Models;
+
+namespace RunGroupWebApp.Data
 {
-    public class ApplicationDbContext
+    public class ApplicationDbContext : DbContext
     {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext>options):base(options)
+        {
+
+        }
+        public DbSet<Club>Clubs  { get; set; }
+        public DbSet<Race> Races { get; set; }
+        public DbSet<Address> Addresses { get; set; }
+
     }
 }
