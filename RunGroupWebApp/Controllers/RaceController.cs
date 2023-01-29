42a66f6c1aa9cb74ed7b8a33f92e749c4fb8d65c
kanwar Dhillon <kdhillon469@gmail.com>
2023-01-29 12:00:13 +1100

    Detail pages added for Clubs and Races

--- RunGroupWebApp/Controllers/RaceController.cs
+++ RunGroupWebApp/Controllers/RaceController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using RunGroupWebApp.Data;
 
 namespace RunGroupWebApp.Controllers
@@ -15,6 +16,11 @@ namespace RunGroupWebApp.Controllers
         {
             var races = _context.Races.ToList();
             return View(races);
+        }
+        public IActionResult Detail(int id)
+        {
+            var race = _context.Races.Include(a => a.Address).FirstOrDefault(c => c.Id == id);
+            return View(race);
         }
     }
 }
